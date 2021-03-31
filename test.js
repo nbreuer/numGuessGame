c40fcb1d158b50dbf9967e04f122437e1270afeb
Norman Breuer <normbreu@gmail.com>
2021-03-31 17:28:15 -0400

    first commit

---
+++ test.js
@@ -0,0 +1,37 @@
+//! what 
+// what 
+//TODO: WHAT
+
+
+console.log(object);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+
+array.forEach(element => {
+    
+});
+
+for (const iterator of object) {
+    
+}
+
+while (condition) {
+    
+}
+
+while
+
+if (condition) {
+    
+} else {
+    
+}
